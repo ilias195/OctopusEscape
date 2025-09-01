d5626cdbf54e1a34db5449d9769de8b1b08ad9b3
Celestial-Fox <stealevw@gmail.com>
2025-09-01 17:28:16 +0200

    hoverEffectScript

---
+++ Assets/scripts/mouse_hover.cs
@@ -0,0 +1,40 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class mouse_hover : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    private bool state;
+    [SerializeField] private GameObject sprite1;
+    [SerializeField] private GameObject sprite2;
+
+    void Update()
+    {
+        
+
+        if (state)
+        {
+            sprite1.SetActive(true);
+            sprite2.SetActive(false);
+            Console.WriteLine("Enter");
+        }
+        else
+        {
+            sprite1.SetActive(false);
+            sprite2.SetActive(true);
+            Console.WriteLine("exit");
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        state = !state;
+    }
+
+    private void OnMouseExit()
+    {
+        state = !state;
+    }
+}
